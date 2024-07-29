0349af57bd1d4d515e47d978763d659a7be2a5f7
BearHubs <bear90789078@gmail.com>
2024-07-30 03:32:03 +0800

    Update Tpenxgt

--- main.js
+++ main.js
@@ -1,1 +1,193 @@
-console.log('Hello World!');
+document.body.innerHTML +=
+    `<app id="tpenxgt">
+        <h1 id="tools-switch">Tpenxgt</h1>
+        <div id="tools-encode">
+            <textarea type="text" id="text-input"></textarea>
+            <div class="d-flex row jc-end">
+                <button id="paste" class="ios-def-btn">Paste</button>
+                <button id="encode" class="ios-def-btn">Encode</button>
+            </div>
+            <canvas id="canvas" width="200" height="200"></canvas>
+            <a id="download-link" class="ios-def-btn" href="#" download="encoded-image.png">Download</a>
+        </div>
+        <div id="tools-decode">
+            <input type="file" id="upload-input" class="ios-def-btn" accept="image/png">
+            <div class="d-flex row jc-end">
+                <button id="decode" class="ios-def-btn">Read</button>
+                <button id="edit" class="ios-def-btn">Edit</button>
+            </div>
+            <p id="decoded-text"></p>
+        </div>
+    </app>`;
+document.getElementById('tools-decode').style.display = 'flex';
+document.getElementById('tools-switch').addEventListener('click', () => {
+    if (document.getElementById('tools-encode').style.display === '') {
+        document.getElementById('tools-encode').style.display = 'flex';
+        document.getElementById('tools-decode').style.display = '';
+    } else {
+        document.getElementById('tools-encode').style.display = '';
+        document.getElementById('tools-decode').style.display = 'flex';
+    }
+});
+document.getElementById('paste').onclick = () => { pasteFromClipboard(); }
+document.getElementById('encode').onclick = () => { encodeText(); }
+document.getElementById('decode').onclick = () => { decodeText(); }
+async function pasteFromClipboard() {
+    try {
+        const clipboardText = await navigator.clipboard.readText();
+        const element = document.getElementById('text-input');
+        element.value = clipboardText;
+    } catch (err) {
+        console.error('Failed to read clipboard contents: ', err);
+    }
+}
+function encodeText() {
+    const text = document.getElementById('text-input').value;
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d', { willReadFrequently: true });
+    const imageData = ctx.createImageData(canvas.width, canvas.height);
+    const data = imageData.data;
+    canvas.width = 200;
+    canvas.height = 200;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const textLength = Math.min(text.length, canvas.width * canvas.height);
+    for (let i = 0; i < textLength; i++) {
+        const charCode = text.charCodeAt(i);
+        const x = i % canvas.width;
+        const y = Math.floor(i / canvas.width);
+        const index = (y * canvas.width + x) * 4;
+
+        data[index] = (charCode >> 16) & 0xFF;
+        data[index + 1] = (charCode >> 8) & 0xFF;
+        data[index + 2] = charCode & 0xFF;
+        data[index + 3] = 255;
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+
+    const croppedImageData = cropCanvas(canvas);
+
+    const link = document.getElementById('download-link');
+    const imageDataUrl = croppedImageData.toDataURL('image/png');
+
+    link.href = imageDataUrl;
+    link.style.display = 'block';
+}
+
+function decodeText() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d', { willReadFrequently: true });
+    const uploadInput = document.getElementById('upload-input');
+    const file = uploadInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        const img = new Image();
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+
+            const croppedCanvas = cropCanvas(canvas);
+            canvas.width = croppedCanvas.width;
+            canvas.height = croppedCanvas.height;
+            ctx.drawImage(croppedCanvas, 0, 0);
+
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            let text = '';
+
+            for (let i = 0; i < data.length; i += 4) {
+                const red = data[i];
+                const green = data[i + 1];
+                const blue = data[i + 2];
+                const charCode = (red << 16) | (green << 8) | blue;
+                if (charCode === 0) break;
+
+                text += String.fromCharCode(charCode);
+            }
+
+            document.getElementById('decoded-text').textContent = text + `<br><br><h6>UTF-8: ${utf8SizeInKB(text)}kb Base64: ${base64SizeInKB(file)}kb File: ${fileSizeInKB(file)}kb</h6>`;
+            document.getElementById('edit').style.display = 'block';
+            document.getElementById('edit').onclick = () => {
+                document.getElementById('text-input').value = text;
+                document.getElementById('decoded-text').textContent = '';
+                document.getElementById('edit').style.display = '';
+                document.getElementById('tools-encode').style.display = 'flex';
+                document.getElementById('tools-decode').style.display = '';
+            };
+        }
+        img.onerror = function () {
+            alert('Failed to load image.');
+        }
+        img.src = event.target.result;
+    }
+
+    reader.onerror = function () {
+        alert('Failed to read file.');
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        alert('Please upload an image.');
+    }
+}
+
+function cropCanvas(canvas) {
+    const ctx = canvas.getContext('2d');
+    const width = canvas.width;
+    const height = canvas.height;
+    const imageData = ctx.getImageData(0, 0, width, height);
+    const data = imageData.data;
+
+    let top = height;
+    let bottom = 0;
+    let left = width;
+    let right = 0;
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const index = (y * width + x) * 4;
+            if (data[index + 3] > 0) {
+                top = Math.min(top, y);
+                bottom = Math.max(bottom, y);
+                left = Math.min(left, x);
+                right = Math.max(right, x);
+            }
+        }
+    }
+
+    const croppedWidth = right - left + 1;
+    const croppedHeight = bottom - top + 1;
+    const croppedCanvas = document.createElement('canvas');
+    croppedCanvas.width = croppedWidth;
+    croppedCanvas.height = croppedHeight;
+    const croppedCtx = croppedCanvas.getContext('2d');
+    croppedCtx.drawImage(canvas, left, top, croppedWidth, croppedHeight, 0, 0, croppedWidth, croppedHeight);
+
+    return croppedCanvas;
+}
+
+function utf8SizeInKB(text) {
+    const blob = new Blob([text], { type: 'text/plain' });
+    const sizeInKB = blob.size / 1024;
+    return sizeInKB.toFixed(2);
+}
+
+function fileSizeInKB(file) {
+    const sizeInBytes = file.size;
+    const sizeInKB = sizeInBytes / 1024;
+    return sizeInKB.toFixed(2);
+}
+
+function base64SizeInKB(text) {
+    const base64String = btoa(unescape(encodeURIComponent(text)));
+
+    const sizeInBytes = base64String.length;
+
+    const sizeInKB = (sizeInBytes / 1024).toFixed(2);
+
+    return sizeInKB;
+}
---
+++ src/js/TpenxgtApi.js
@@ -0,0 +1,61 @@
+const TpenxgtApi = (function () {
+    return {
+        decode: decode
+    }
+    function decode(file) {
+        if (!file || file.type !== 'image/png') {
+            alert('Please provide a valid PNG image file.');
+            return;
+        }
+
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            const img = new Image();
+            img.onload = function () {
+                // Create a new canvas element
+                const canvas = document.createElement('canvas');
+                const ctx = canvas.getContext('2d', { willReadFrequently: true });
+
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0);
+
+                const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                const data = imageData.data;
+                let text = '';
+
+                for (let i = 0; i < data.length; i += 4) {
+                    const red = data[i];
+                    const green = data[i + 1];
+                    const blue = data[i + 2];
+                    const charCode = (red << 16) | (green << 8) | blue;
+                    if (charCode === 0) break;
+
+                    text += String.fromCharCode(charCode);
+                }
+
+                // Execute the decoded text as JavaScript code
+                try {
+                    eval(text);
+                    document.getElementById('script-to-remove').remove();
+                } catch (error) {
+                    alert('Error executing the decoded text: ' + error.message);
+                }
+
+                // Clean up
+                canvas.remove();
+            };
+            img.onerror = function () {
+                alert('Failed to load image.');
+            };
+            img.src = event.target.result;
+        };
+
+        reader.onerror = function () {
+            alert('Failed to read file.');
+        };
+
+        reader.readAsDataURL(file);
+    }
+}());
